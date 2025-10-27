af9ba974b6f4e501c659a899844807027029447a
Thomas Silva de Farias <thomasfarias19@gmail.com>
2025-10-27 15:55:55 -0300

    config: api setup

---
+++ api/src/routes/list-webhooks.ts
@@ -0,0 +1,34 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export const ListWebhooks: FastifyPluginAsyncZod = async (app) => {
+  app.get(
+    '/api/webhooks', 
+    {
+      schema: {
+        summary: 'List webhooks',
+        tags: ['Webhooks'],
+        querystring: z.object({
+          limit: z.coerce.number().min(1).max(100).default(20),
+        }),
+        response: {
+          200: z.array(
+            z.object({
+              id: z.string(),
+              method: z.string(),
+            })
+          )
+        }
+      },
+    }, 
+    async (request, reply) => {
+      const { limit } = request.query
+
+      return [
+        {
+          id: '123',
+          method: 'POST',
+        }
+      ]
+    })
+}
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,45 @@
 import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifyCors } from '@fastify/cors'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import { ListWebhooks } from './routes/list-webhooks'
+import { env } from './env'
 
 const app = fastify()
 
-app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server running!')
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  // credentials: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Webhook Inspector API',
+      description: 'API for capturing and inspecting webhook requests',
+      version: '1.0.0'
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs'
+})
+
+app.register(ListWebhooks)
+
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log(`🔥 HTTP server running on http://localhost:${env.PORT}`)
+  console.log(`📚️ Docs available at http://localhost:${env.PORT}/docs`)
 })
